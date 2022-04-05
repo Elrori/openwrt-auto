0cc2ab42f64bbe1ac3b9b242e9d18530a0cf45bb
elrori <zhouchaoyu2013@icloud.com>
2022-04-05 21:37:13 +0800

    add 19.07.3 patches

--- scripts-tools/make-ebaz4205.sh
+++ scripts-tools/make-ebaz4205.sh
@@ -2,17 +2,19 @@
 CONFIG_FILE=zynq-ebaz4205-lite.config
 DEVICE_NAME=zynq-ebaz4205
 TARGET_NAME=zynq
-CONFIG_TAGS='v19.07.3'
+CONFIG_TAGS='v19.07.3' # v19.07 was a stable version for ebaz4205, modifications are not recommended
+TOP_DIR=$PWD
 
 # Get openwrt source master
 echo "-----------> Get openwrt source master"
 if [ ! -d "openwrt" ]; then
-  git clone -b $CONFIG_TAGS https://github.com/openwrt/openwrt.git
+  git clone --depth 1 -b $CONFIG_TAGS https://github.com/openwrt/openwrt.git
 else
   echo "Pass"
 fi
-cd openwrt
-echo `pwd`
+echo "Top dir: $TOP_DIR"
+pushd openwrt
+echo "Enter: $PWD"
 
 # Get kernel version
 echo "-----------> Get kernel version"
@@ -29,12 +31,14 @@ echo "KERNEL_VERSION=$KERNEL_VERSION$LINUX_VERSION"
 # Add EBAZ4205 patches
 echo "-----------> Add EBAZ4205 patches"
 mkdir -p target/linux/zynq/$PATCHES
-cp ../../patches/ebaz4205-v5.10-u-boot-2019.07/020-v5.10-ebaz4205-support.patch target/linux/zynq/$PATCHES
-cp ../../patches/ebaz4205-v5.10-u-boot-2019.07/111-u-boot-2019.07-ebaz4205-support.patch package/boot/uboot-zynq/patches
-git apply ../../patches/ebaz4205-v5.10-u-boot-2019.07/openwrt-ebaz4205.patch
+cp $TOP_DIR/patches/ebaz4205-v4.14-u-boot-2018.07-19.07.3/020-v4.14-ebaz4205-support.patch target/linux/zynq/$PATCHES
+cp $TOP_DIR/patches/ebaz4205-v4.14-u-boot-2018.07-19.07.3/111-u-boot-2018.07-ebaz4205-support.patch package/boot/uboot-zynq/patches
+git apply $TOP_DIR/patches/ebaz4205-v4.14-u-boot-2018.07-19.07.3/openwrt-ebaz4205-19.07.3.patch
 echo "Apply patches to target/linux/zynq/$PATCHES!"
 
-## Add luci-app-ssr-plus
+
+
+# Add luci-app-ssr-plus
 #echo "-----------> Add luci-app-ssr-plus"
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
@@ -46,49 +50,29 @@ echo "Apply patches to target/linux/zynq/$PATCHES!"
 #sed -i '41a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
 #./scripts/feeds update -a
 #./scripts/feeds install -a
-#
-## Add config from https://github.com/Elrori/openwrt-auto.git
-#echo "-----------> Add config from https://github.com/Elrori/openwrt-auto.git"
-#cp ../../config/$CONFIG_FILE .config
-#
-## make download
-#echo "-----------> make download"
-#make defconfig
-#make download -j$(nproc)
-#
-## make compile
-#echo "-----------> make compile"
-#make -j$(nproc) V=sc
-#
-## make image from zed board
-#echo "-----------> make image from zed board"
-#mkdir artifacts
-#svn checkout https://github.com/Elrori/EBAZ4205/trunk/archive/2018.3-1 artifacts/fpga
-#cp artifacts/fpga/zynq-ebaz4205.dts build_dir/target-*/linux-zynq/linux-$KERNEL_VERSION/arch/arm/boot/dts/zynq-zed.dts
-#make -j$(nproc) V=sc
-#
-#echo "-----------> 重新编译成功,开始制作ebaz4205镜像"
-#echo -e "bootargs=console=ttyPS0,115200n8 root=/dev/mmcblk0p2 rootwait earlyprintk \n\
-#uenvcmd=run sdbootfit \n\
-#sdbootfit=echo Run uEnv.txt copying Linux from SD to RAM... && \n\
-#fatload mmc 0 0x1000000 fit.itb && \n\
-#echo Boot fit.itb from RAM && \n\
-#bootm 0x1000000;" > artifacts/uEnv.txt
-#cp build_dir/target-*/linux-zynq/Image artifacts/
-#cp build_dir/target-*/linux-zynq/linux-$KERNEL_VERSION/arch/arm/boot/dts/zynq-zed.dtb artifacts/zynq-ebaz4205.dtb
-#cp build_dir/target-*/linux-zynq/root.ext4 artifacts/
-#gzip -f -9n -c artifacts/Image > artifacts/Image.gz
-#scripts/mkits.sh -D ebang_zynq-ebaz4205 -o $(pwd)/artifacts/fit.its -k $(pwd)/artifacts/Image.gz -C gzip   -d $(pwd)/artifacts/zynq-ebaz4205.dtb   -a 0x8000 -e 0x8000    -c "config-1" -A arm -v $KERNEL_VERSION
-#staging_dir/host/bin/mkimage -f $(pwd)/artifacts/fit.its $(pwd)/artifacts/fit.itb
-#staging_dir/host/bin/mkfs.fat $(pwd)/artifacts/boot -C 16384
-#staging_dir/host/bin/mcopy -i $(pwd)/artifacts/boot $(pwd)/artifacts/fpga/BOOT.bin ::boot.bin
-#staging_dir/host/bin/mcopy -i $(pwd)/artifacts/boot $(pwd)/artifacts/uEnv.txt ::uEnv.txt
-#staging_dir/host/bin/mcopy -i $(pwd)/artifacts/boot $(pwd)/artifacts/fit.itb ::fit.itb
-#export PATH=$PATH:staging_dir/host/bin
-#target/linux/zynq/image/gen_zynq_sdcard_img.sh $(pwd)/artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz $(pwd)/artifacts/boot $(pwd)/artifacts/root.ext4 16 512
-#gzip -f -9n -c artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz > artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz.new
-#rm -rf artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz
-#mv artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz.new artifacts/openwrt-zynq-ebang_zynq-ebaz4205-ext4-sdcard-precompileduboot.img.gz
-#rm -rf artifacts/Image artifacts/boot artifacts/fit.its artifacts/fpga artifacts/root.ext4
-#
-#echo "-----------> SUCCESS"
+
+
+
+# Add config from https://github.com/Elrori/openwrt-auto.git
+echo "-----------> Add config from https://github.com/Elrori/openwrt-auto.git"
+cp $TOP_DIR/config/$CONFIG_FILE .config
+
+# make download
+echo "-----------> make download"
+make defconfig
+make download -j$(nproc)
+
+# make compile
+echo "-----------> make compile"
+make -j$(nproc) V=sc
+
+# make image from zed board
+echo "-----------> Pack artifacts"
+mkdir artifacts
+for file in bin/targets/*/*/*.gz; do
+  sudo bash -c "sha512sum $file | sed -r 's|([0-9a-z]+).*|\1|g' > $file.sha512sum"
+  sudo mv $file* artifacts/
+done
+
+echo "-----------> Finish"
+popd
