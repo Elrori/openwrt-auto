e1ddd40a099cd6bb62bcbfec426f10badf6bb02f
elrori <zhouchaoyu2013@icloud.com>
2022-04-06 11:41:27 +0800

    modify

--- scripts-tools/make-ebaz4205.sh
+++ scripts-tools/make-ebaz4205.sh
@@ -5,8 +5,7 @@ TARGET_NAME=zynq
 CONFIG_TAGS='v19.07.3' # v19.07 was a stable version for ebaz4205, modifications are not recommended
 TOP_DIR=$PWD
 
-# Get openwrt source master
-echo "-----------> Get openwrt source master"
+echo "----------------------> Get openwrt source master"
 if [ ! -d "openwrt" ]; then
   git clone --depth 1 -b $CONFIG_TAGS https://github.com/openwrt/openwrt.git
 else
@@ -16,8 +15,8 @@ echo "Top dir: $TOP_DIR"
 pushd openwrt
 echo "Enter: $PWD"
 
-# Get kernel version
-echo "-----------> Get kernel version"
+
+echo "----------------------> Get kernel version"
 KERNEL_VERSION=$(cat target/linux/$TARGET_NAME/Makefile | grep KERNEL_PATCHVER | sed -r 's|.*([0-9]+.[0-9]+)$|\1|')
 PATCHES=patches-$KERNEL_VERSION
 if [ ! -e "include/kernel-$KERNEL_VERSION" ]; then
@@ -28,8 +27,8 @@ fi
 LINUX_VERSION=$(cat include/$VERSION_FILE | grep LINUX_VERSION-$KERNEL_VERSION | sed -r 's|.*(\.[0-9]+)$|\1|')
 echo "KERNEL_VERSION=$KERNEL_VERSION$LINUX_VERSION"
 
-# Add EBAZ4205 patches
-echo "-----------> Add EBAZ4205 patches"
+
+echo "----------------------> Add EBAZ4205 patches"
 mkdir -p target/linux/zynq/$PATCHES
 cp $TOP_DIR/patches/ebaz4205-v4.14-u-boot-2018.07-19.07.3/020-v4.14-ebaz4205-support.patch target/linux/zynq/$PATCHES
 cp $TOP_DIR/patches/ebaz4205-v4.14-u-boot-2018.07-19.07.3/111-u-boot-2018.07-ebaz4205-support.patch package/boot/uboot-zynq/patches
@@ -38,8 +37,8 @@ echo "Apply patches to target/linux/zynq/$PATCHES!"
 
 
 
-# Add luci-app-ssr-plus
-#echo "-----------> Add luci-app-ssr-plus"
+
+#echo "----------------------> Add luci-app-ssr-plus"
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
@@ -48,31 +47,35 @@ echo "Apply patches to target/linux/zynq/$PATCHES!"
 #done
 #sed -i '23a\tools-y += ucl upx' tools/Makefile # 使用sed插入特定行，在未来可能会出现问题
 #sed -i '41a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
-#./scripts/feeds update -a
-#./scripts/feeds install -a
 
 
 
-# Add config from https://github.com/Elrori/openwrt-auto.git
-echo "-----------> Add config from https://github.com/Elrori/openwrt-auto.git"
-cp $TOP_DIR/config/$CONFIG_FILE .config
 
-# make download
-echo "-----------> make download"
+echo "----------------------> Get feeds"
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+
+echo "----------------------> Add config from elrori/openwrt-auto"
+make menuconfig
+#cp $TOP_DIR/config/$CONFIG_FILE .config
+
+
+echo "----------------------> make download"
 make defconfig
 make download -j$(nproc)
 
-# make compile
-echo "-----------> make compile"
+
+echo "----------------------> make compile"
 make -j$(nproc) V=sc
 
-# make image from zed board
-echo "-----------> Pack artifacts"
+
+echo "----------------------> Pack artifacts"
 mkdir artifacts
 for file in bin/targets/*/*/*.gz; do
   sudo bash -c "sha512sum $file | sed -r 's|([0-9a-z]+).*|\1|g' > $file.sha512sum"
   sudo mv $file* artifacts/
 done
 
-echo "-----------> Finish"
+echo "----------------------> Finish"
 popd
