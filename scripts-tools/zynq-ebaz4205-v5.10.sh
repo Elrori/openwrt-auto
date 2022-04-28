822ed5e8a156d62f32a766c7e65d51d4ea167462
elrori <zhouchaoyu2013@icloud.com>
2022-04-28 08:57:59 +0800

    Delete zynq-ebaz4205-v5.10.sh

--- scripts-tools/zynq-ebaz4205-v5.10.sh
+++
@@ -1,3 +0,0 @@
-cp openwrt-auto/patches/ebaz4205-v5.10-u-boot-2019.07/020-v5.10-ebaz4205-support.patch target/linux/zynq/${{ env.PATCHES_FILE }}/
-cp openwrt-auto/patches/ebaz4205-v5.10-u-boot-2019.07/111-u-boot-2019.07-ebaz4205-support.patch package/boot/uboot-zynq/patches/
-git apply openwrt-auto/patches/ebaz4205-v5.10-u-boot-2019.07/openwrt-ebaz4205.patch
